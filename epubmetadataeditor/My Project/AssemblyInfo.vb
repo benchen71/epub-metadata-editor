a9304723fe42d66f8664b84c908d423735dfe000
Ben <benchen71@server.fake>
2013-12-13 20:43:10 +1100

    Version 1.2.18

--- epubmetadataeditor/My Project/AssemblyInfo.vb
+++ epubmetadataeditor/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.17")> 
-<Assembly: AssemblyFileVersion("1.2.17")> 
+<Assembly: AssemblyVersion("1.2.18")> 
+<Assembly: AssemblyFileVersion("1.2.18")> 
